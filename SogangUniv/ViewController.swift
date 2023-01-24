e09fb59cb4563b20bcd641a5c2d9f7885b82efc2
LeeDanny06 <leedenny06@gmail.com>
2023-01-25 01:00:28 +0900

    Initial Commit

---
+++ SogangUniv/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SogangUniv
+//
+//  Created by Daniel on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
